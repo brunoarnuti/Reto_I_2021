ad4445cf6e83dab37b40c404dce0f8d9b559c905
brunoarnuti <brunoarnuti@gmail.com>
2021-02-24 15:47:29 -0300

    Fixing main

--- RetoI/RetoI/bioinformatica/models/__init__.py
+++ RetoI/RetoI/bioinformatica/models/__init__.py
@@ -2,4 +2,5 @@
 from . import client
 from . import project
 from .experiment import Experiment
+from .dinamicattribute import DynamicAttribute
 
---
+++ RetoI/RetoI/bioinformatica/models/dinamicattribute.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class DynamicAttribute(models.Model):
+
+    TYPE1 = 'T1'
+    TYPE2 = 'T2'
+    TYPE3 = 'T3'
+
+    ATTRIBUTE_TYPES_CHOICES = [
+        ('TYPE1', 'Type 1'),
+        ('TYPE2', 'Type 2'),
+        ('TYPE3', 'Type 3'),
+    ]
+    attribute_id = models.CharField(max_length=120)
+    attribute_type = models.CharField(max_length=5, choices=ATTRIBUTE_TYPES_CHOICES, default=TYPE1)
+    attribute_value = models.CharField(max_length=200)
+
