82022518b412ca33d60cdc1d9746c1d0c55d8618
juanizquierdo19 <jpizquierdo19@gmail.com>
2021-02-24 20:06:33 -0300

    created sample model

---
+++ RetoI/RetoI/bioinformatica/admin/sampleAdmin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from bioinformatica.models.project import Sample
+
+admin.site.register(Sample)
---
+++ RetoI/RetoI/bioinformatica/models/sample.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Sample(models.Model):
+    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE)
+    date_created = models.DateTimeField('date created', auto_now_add=True, editable=True)
+    technician = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.experiment.name
