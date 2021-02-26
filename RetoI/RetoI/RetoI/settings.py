03507d4d338f984ec0cfdde45af8da4d803254b3
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-25 21:24:44 -0300

    prueba 2

--- RetoI/RetoI/RetoI/settings.py
+++ RetoI/RetoI/RetoI/settings.py
@@ -76,7 +76,6 @@ WSGI_APPLICATION = 'RetoI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
