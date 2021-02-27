24dbafd1ea5ef7c52acabc398ab9afad373125b4
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-27 13:24:44 -0300

    FIX projectAdmin

--- RetoI/RetoI/bioinformatica/admin/projectAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/projectAdmin.py
@@ -1,32 +1,24 @@
 from django.contrib import admin
 from bioinformatica.models.project import Project
 from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
-from bioinformatica.models.experiment import Experiment
+from bioinformatica.admin.experimentAdmin import ExperimentInline
+from bioinformatica.models.logicaldelete import LogicaLDeletedModelTabularInLine
 
 
-class AttributeInlineExperiment(admin.TabularInline):
-    model = Experiment
-    extra = 0
-    classes = ['collapse']
-
-class AttributeInlineProject(admin.TabularInline):
+class ProjectInline(LogicaLDeletedModelTabularInLine):
     model = Project
     extra = 0
-    classes = ['collapse']
 
 class ProjectAdmin(LogicalDeletedModelAdmin):
     list_display = ('name','contact','create_date')
-    list_filter = ['name','contact','create_date']
+    list_filter = ['create_date']
     search_fields = ['name','contact__name', 'contact__surname',]
-    inlines = [AttributeInlineExperiment,AttributeInlineProject]
+    inlines = [ExperimentInline,ProjectInline]
 
     fieldsets = [
         (None, {'fields': [('name','contact')]}),
         ('Project information', {'fields': ['create_date','description','projects'],'classes':['collapse']}),
     ]
-
-class projectAdmin(admin.ModelAdmin):
-    exclude = ['deleted', ]
 
 admin.site.register(Project,ProjectAdmin)
 
