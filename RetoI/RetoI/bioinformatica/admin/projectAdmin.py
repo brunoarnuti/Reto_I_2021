eaa546d8dadcbea595d0fcd766b7b6ed7f97fef4
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-26 21:11:22 -0300

    FIX solo dejamos filtro por fecha en projects de Admin

--- RetoI/RetoI/bioinformatica/admin/projectAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/projectAdmin.py
@@ -16,7 +16,7 @@ class AttributeInlineProject(admin.TabularInline):
 
 class ProjectAdmin(LogicalDeletedModelAdmin):
     list_display = ('name','contact','create_date')
-    list_filter = ['name','contact','create_date']
+    list_filter = ['contact','create_date']
     search_fields = ['name','contact__name', 'contact__surname',]
     inlines = [AttributeInlineExperiment,AttributeInlineProject]
 
