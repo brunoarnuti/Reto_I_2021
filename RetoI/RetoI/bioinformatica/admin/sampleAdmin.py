96df3751dc952159bb25adf5b3f374a60edf8341
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-27 13:25:27 -0300

    FIX SampleAdmin

--- RetoI/RetoI/bioinformatica/admin/sampleAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/sampleAdmin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from bioinformatica.models.sample import Sample
+from bioinformatica.models.logicaldelete import LogicaLDeletedModelTabularInLine, LogicalDeletedModelAdmin
 
 
-class SampleAdmin(admin.ModelAdmin):
-    exclude = ['deleted', ]
+class SampleAdmin(LogicalDeletedModelAdmin):
+     list_display = ('name','responsible','location')
 
+class SampleInline(LogicaLDeletedModelTabularInLine):
+    model = Sample
+    extra = 0
 
 admin.site.register(Sample, SampleAdmin)
