c71fb5d59d227a5c65b7027e7c3cd80fa9bd4361
brunoarnuti <brunoarnuti@gmail.com>
2021-02-27 10:08:59 -0300

    Agrego searchfields a lo que faltaba

--- RetoI/RetoI/bioinformatica/admin/fastQAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/fastQAdmin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from bioinformatica.models.fastQ import FastQ, FastQFile
+from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
 
 
+class FastQAdmin(LogicalDeletedModelAdmin):
+    search_fields = ['date_created']
 
-class FastQAdmin(admin.ModelAdmin):
-    exclude = ['deleted', ]
 
 
 admin.site.register(FastQ, FastQAdmin)
