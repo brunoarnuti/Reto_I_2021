680069d2e41bb5ef4cc09dae1ece6aefe9015e8c
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-27 12:45:00 -0300

    FIX clientAdmin

--- RetoI/RetoI/bioinformatica/admin/clientAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/clientAdmin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from bioinformatica.models.client import Client
-from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
+from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin, LogicaLDeletedModelTabularInLine
+from bioinformatica.admin.projectAdmin import ProjectInline
 
+
+class ClientInline(LogicaLDeletedModelTabularInLine):
+    model = Client
+    extra = 0
 
 class clientAdmin(LogicalDeletedModelAdmin):
-    exclude = ['deleted', ]
+    inlines = [ProjectInline,ClientInline]
+    exclude = ['project_ids','contact_ids']
+    list_display = ('name','company','phone')
 
 
 admin.site.register(Client,clientAdmin)
