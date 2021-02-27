a7a67d03bd72c4c1b1ee20977e24dc86dc9f31e7
Emmanuel Acosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-02-27 16:13:39 -0300

    Se agrega location = True y el graypy en requirements

--- RetoI/RetoI/bioinformatica/models/experiment.py
+++ RetoI/RetoI/bioinformatica/models/experiment.py
@@ -11,7 +11,7 @@ class Experiment(LogicalDeletedModel):
     name = models.CharField(max_length=120)
     place = models.CharField(max_length=200)
     date = models.DateTimeField('date published', default=timezone.now)
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200,blank=True)
     state = models.CharField(max_length=120)
     project_id = models.ForeignKey('Project', on_delete=models.DO_NOTHING, blank=True, null=True)
     logger.info("[INFO] Primer log")
