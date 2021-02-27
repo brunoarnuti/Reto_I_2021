dea635ea82409fab587ad61a41f730049ef02cca
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-02-27 13:22:36 -0300

    FIX FastQAdmin

--- RetoI/RetoI/bioinformatica/admin/fastQAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/fastQAdmin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from bioinformatica.models.fastQ import FastQ, FastQFile
+from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
 
 
-
-class FastQAdmin(admin.ModelAdmin):
-    exclude = ['deleted', ]
-
+class FastQAdmin(LogicalDeletedModelAdmin):
+    pass
 
 admin.site.register(FastQ, FastQAdmin)
