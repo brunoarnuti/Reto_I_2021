1f54ec460debf2c1f68bdaab46e534f751050a0f
brunoarnuti <brunoarnuti@gmail.com>
2021-02-27 10:09:58 -0300

    Elimino el Published en experiment

--- RetoI/RetoI/bioinformatica/admin/experimentAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/experimentAdmin.py
@@ -1,30 +1,23 @@
 from django.contrib import admin
-
-from bioinformatica.models import DynamicAttributeInstance
 from bioinformatica.models.experiment import Experiment
-
-# Register your models here.
-
-
-class AttributeInline(admin.TabularInline):
-    model = DynamicAttributeInstance
-    extra = 1
-    fields = ['attribute_type', 'attribute_value',]
+from bioinformatica.admin.dinamicattributeAdmin import AttributeInline
+from bioinformatica.admin.sampleAdmin import SamplesInline
+from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
 
 
-class ExperimentAdmin(admin.ModelAdmin):
+class ExperimentAdmin(LogicalDeletedModelAdmin):
+
     fieldsets = [
         (None, {'fields': ['name']}),
-        ('Date information', {'fields': ['date']}),
         ('Project id', {'fields': ['project_id']})
     ]
-    list_display = ('name','date','place','state','project_id',)
-    list_filter = ['date']
+
+    list_display = ('name', 'place', 'state', 'project_id',)
+
+    inlines = [SamplesInline, AttributeInline]
+
     search_fields = ['name']
-    inlines = [AttributeInline]
 
-class experimentAdmin(admin.ModelAdmin):
-    exclude = ['deleted', ]
 
-admin.site.register(Experiment,ExperimentAdmin)
+admin.site.register(Experiment, ExperimentAdmin)
 
--- RetoI/RetoI/bioinformatica/models/experiment.py
+++ RetoI/RetoI/bioinformatica/models/experiment.py
@@ -6,7 +6,6 @@ from .logicaldelete import LogicalDeletedModel
 class Experiment(LogicalDeletedModel):
 
     name = models.CharField(max_length=120)
-    date = models.DateTimeField('date published', default=timezone.now)
     place = models.CharField(max_length=200)
     state = models.CharField(max_length=120)
     project_id = models.ForeignKey('Project', on_delete=models.DO_NOTHING, blank=True, null=True)
