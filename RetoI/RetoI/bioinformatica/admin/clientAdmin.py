f02bcbe826dd9d35db6a0171502ffb4bc4447061
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-03-01 19:53:27 -0300

    FIX clientAdmin: se define ClientInLine

--- RetoI/RetoI/bioinformatica/admin/clientAdmin.py
+++ RetoI/RetoI/bioinformatica/admin/clientAdmin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from bioinformatica.models.client import Client
-from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin
+from bioinformatica.models.logicaldelete import LogicalDeletedModelAdmin, LogicaLDeletedModelTabularInLine
 
+
+class ClientInline(LogicaLDeletedModelTabularInLine):
+    model = Client
+    extra = 0
+    classes = ['collapse']
 
 class clientAdmin(LogicalDeletedModelAdmin):
     search_fields = ['name', 'surname']
+    inlines = [ClientInline] #agregar projectInLines
 
 
 admin.site.register(Client, clientAdmin)
