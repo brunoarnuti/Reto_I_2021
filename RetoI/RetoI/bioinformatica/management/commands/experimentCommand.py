193be07413bb060994701a44c6f09a414fd9b234
Emmanuel Acosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-03-08 21:12:07 -0300

    Van cambios en experimentCommand relacionados al metodo get_absolute_path

--- RetoI/RetoI/bioinformatica/management/commands/experimentCommand.py
+++ RetoI/RetoI/bioinformatica/management/commands/experimentCommand.py
@@ -12,30 +12,52 @@ class Command(BaseCommand):
         parser.add_argument('--experiment_id')
         parser.add_argument('--location', default='Montevideo')
 
+    def get_absolute_path(self,experiment_id,project_id,filter_var=False):
+        path_list = []
+        p = subprocess.Popen('pwd', shell=True, stdout=subprocess.PIPE)
+        #p = subprocess.Popen('echo %cd%', shell=True, stdout=subprocess.PIPE)
+        out = p.communicate()[0].decode("utf-8").rstrip('\r\n') + '\\media\\UploadedFiles\\PROJ_' + str(
+            project_id) + '\\EXP_' + str(experiment_id) + '\\'
+        out = out.replace('\\', '/')
+        d = []
+        for file in os.listdir(out.rstrip('\r\n')):
+            r = os.path.join(out, file)
+            d.append(r)
+        for direccion in d:
+            for base, dirs, files in os.walk(direccion):
+                base = base.replace('\\', '/')
+                if not filter_var:
+                    path_list.append(base)
+                else:
+                    for file in files:
+                        if file.__contains__('archivo_0.txt'):
+                            path_list.append(base + '/' + file)
+
+
+        return path_list
+
+
+
     def handle(self, *args, **options):
         # if options['command'] == 'agrupar':
         exp_id = options['experiment_id']
+        project_id = options['project_id']
         print(options['command'].split('\r\n'))
         list_commands = options['command'].split('\r\n')
         for command in list_commands:
             if command.__contains__('cat'):
-                p = subprocess.Popen('pwd', shell=True, stdout=subprocess.PIPE)
-                out = p.communicate()[0].decode("utf-8").rstrip('\r\n') + '\\media\\UploadedFiles\\PROJ_' + str(
-                    options['project_id']) + '\\EXP_' + str(options['experiment_id']) + '\\'
-                out = out.replace('\\', '/')
-                d = []
-                for file in os.listdir(out.rstrip('\r\n')):
-                    r = os.path.join(out, file)
-                    d.append(r)
-                for direccion in d:
-                    for base, dirs, files in os.walk(direccion):
-                        base = base.replace('\\', '/')
-                        listBase = base.split('/')
-                        if listBase[len(listBase) - 1].startswith('FQ') and len(base) != 0:
-                            print(base + ' El popen se ejecuta acá')
-                            command = 'cat *.txt > archivo_0.txt'
-                            subprocess.Popen(command, shell=True, cwd=base)
-            elif command.__contains__('nextflow'):
-                print()
-            else:
-                subprocess.Popen(command, shell=True)
+                path_list = self.get_absolute_path(exp_id,project_id)
+                for base in path_list:
+                    #command = 'for %a in (*.txt) do type “%a” >> archivo_0.txt'
+                    command = 'cat *.txt > archivo_0.txt'
+                    subprocess.Popen(command, shell=True, cwd=base)
+            if command.__contains__('nextflow'):
+                main_nf = os.environ.get("MAIN_NF")
+                db_16s = os.environ.get("DB_16S")
+                db_18s = os.environ.get("DB_18S")
+                db_tax = os.environ.get("DB_TAX")
+                list_path = self.get_absolute_path(exp_id,project_id,filter_var=True)
+                for path in list_path:
+                    print("nextflow run %s -profile docker --reads %s  --db %s --tax %s" %(main_nf,path,db_16s,db_tax))
+            #else:
+            #    subprocess.Popen(command, shell=True)
